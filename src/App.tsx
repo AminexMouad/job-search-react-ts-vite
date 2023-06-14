8ca751eca6ba3a9c04b0be541000a46febedaabe
AminexMouad <aminemouad1998@gmail.com>
2023-06-14 20:51:01 +0100

    add React query devtools

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import Routing from './routes';
 import { ThemeProvider } from '@mui/material';
 import theme from './theme/theme';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <QueryClientProvider client={new QueryClient({})}>
         <Routing />
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </ThemeProvider>
   );
