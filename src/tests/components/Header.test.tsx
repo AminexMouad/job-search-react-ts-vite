0d5bcc7e88bff3356d52bbaf6ab9cd98aecec119
AminexMouad <aminemouad1998@gmail.com>
2023-06-19 19:18:45 +0100

    fix header unit test

--- src/tests/components/Header.test.tsx
+++ src/tests/components/Header.test.tsx
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import Header from '../../components/Header';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 describe('Header', () => {
   it('renders correctly', () => {
-    render(<Header />);
+    render(
+      <QueryClientProvider client={new QueryClient({})}>
+        <Header />
+      </QueryClientProvider>
+    );
 
     const logoText = screen.getByText(/Job/);
     const logoText2 = screen.getByText(/Finder/);
